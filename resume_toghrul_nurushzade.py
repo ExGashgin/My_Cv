409d0b4773c113ac409c0c963c22f2f7003290d5
ExGashgin <132515907+ExGashgin@users.noreply.github.com>
2025-05-15 10:42:08 +0200

    Update resume_toghrul_nurushzade.py

--- resume_toghrul_nurushzade.py
+++ resume_toghrul_nurushzade.py
@@ -1,32 +1,37 @@
 import streamlit as st
 
 # --- PAGE SETUP ---
+
+
 about_page = st.Page(
     "views/about_me.py",
     title="About Me",
     icon=":material/account_circle:",
     default=True,
 )
-project_1_page = st.Page(
+
+skills_qualifications = st.Page(
+    "views/skills_qualifications.py",
+    title="Skills & Qualifications",
+    icon=":material/star:",
+)
+study = st.Page(
     "views/study.py",
     title="Study",
     icon=":material/school:",
 )
-project_2_page = st.Page(
+experience = st.Page(
     "views/experience.py",
     title="Experience",
     icon=":material/work_history:",
 )
 
 
-
-
-
 # --- NAVIGATION SETUP [WITH SECTIONS]---
 pg = st.navigation(
     {
-        "Info": [about_page],
-        "Projects": [project_1_page, project_2_page],
+        "Info": [about_page, skills_qualifications],
+        "Study and Experience": [study, experience],
     }
 )
 
